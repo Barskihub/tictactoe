577bcc45aeebf00c831acf4dbb6673b39f3e6867
Arthur Barski <barski.10877716@gmail.com>
2019-05-26 18:53:59 +0300

    First commit
    Add Js sctructure

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 //Данные игры
 let game = {
     isClicked: false,
@@ -10,3 +9,37 @@ let game = {
 };
 
 //
+const takeTheInput = data => {
+    if(!game.isClicked){
+        data.innerHTML = game.gamerX;
+        getData(data, game.cross, game.gamerX);
+        game.isClicked = !game.isClicked
+    }else if(game.isClicked){
+        data.innerHTML = game.gamerO;
+        getData(data, game.round, game.gamerO);
+        game.isClicked = !game.isClicked
+    }
+};
+
+const getData = (data, whichGame, player) => {
+    let box = getData.getAttribute('data-box');
+
+    if(!game.isClicked){
+        game.cross.push(box)
+    }else if(game.isClicked){
+        game.round.push(box)
+    }
+    data.removeAttribute('onclick');
+    checkMoves(whichGame, player)
+};
+
+const checkMoves = (arr, player) => {
+    let compare = arr.map(v => parseInt(v, 10)).sort();
+    for(let i = 0; i < game.winningArray.length; i++){
+        if(
+            compare.includes()
+        ){
+
+        }
+    }
+}
