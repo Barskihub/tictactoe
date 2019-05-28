208e13e956dedf3614bba890411cb1cc6ef67e84
Arthur Barski <barski.10877716@gmail.com>
2019-05-28 13:29:01 +0300

    -1 bug and UI
    - 1 bug with input and start UI

--- js/script.js
+++ js/script.js
@@ -1,27 +1,10 @@
-//
+// Обьявим все нужные переменные
 let gamePlace = document.getElementById('gamePlace');
 let turn = 0;
 let turnWho = document.getElementById('turnWho');
 let box = document.getElementsByClassName('box');
 let result = document.getElementById('result');
-
-gamePlace.onclick = e => {
-    if (e.target.className === 'box') {
-        if (turn % 2 === 0) {
-            e.target.innerHTML = 'O';
-            turnWho.innerHTML = 'Ход крестиков (Х)'
-        } else {
-            e.target.innerHTML = 'X';
-            turnWho.innerHTML = 'Ход ноликов (0)'
-        }
-        turn++;
-        checkWinner();
-    }
-};
-// checkWinner = () => {
-//     console.log(box);
-// }
-
+// Обьявим массив победных комбинаций
 let winningArray = [
     [0, 1, 2],
     [3, 4, 5],
@@ -32,12 +15,38 @@ let winningArray = [
     [2, 4, 6],
     [0, 4, 8]
 ];
+// Функция, при клике добавляется либо Х, либо О, в зависимости от хода
+gamePlace.onclick = e => {
+    if (e.target.className === 'box') {
+        if (turn % 2 === 0) {
+            console.log(e.target);
+            e.target.innerHTML = 'X';
+            turnWho.innerHTML = 'Ход ноликов (O)';
+            e.target.className = 'box clicked';
+        } else {
+            e.target.innerHTML = 'O';
+            turnWho.innerHTML = 'Ход крестиков (Х)';
+            e.target.className = 'box clicked';
+        }
+        turn++;
+        checkWinner();
+    }
+};
 
+
+//Проверка игровых клеток и сравнение их с winningArray
 checkWinner = () => {
     for (let i = 0; i<winningArray.length; i++){
         if (box[winningArray[i][0]].innerHTML === 'X' && box[winningArray[i][1]].innerHTML === 'X' && box[winningArray[i][2]].innerHTML === 'X') {
-            result.innerHTML += 'крестики';
+            result.innerHTML += 'Победили крестики!';
+            turnWho.style.display = 'none';
             console.log('Победили крестики')
+        } else if (box[winningArray[i][0]].innerHTML === 'O' && box[winningArray[i][1]].innerHTML === 'O' && box[winningArray[i][2]].innerHTML === 'O') {
+            result.innerHTML += 'Победили нолики!';
+            turnWho.style.display = 'none';
+            console.log('Победили нолики')
+        } else if(gamePlace.length === 9)  {
+            console.log('Ничья');
         }
     }
-}
+};
