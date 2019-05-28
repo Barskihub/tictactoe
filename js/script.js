b5c01691e88a7fe339b3ac62828d39a3f4b6942d
Arthur Barski <barski.10877716@gmail.com>
2019-05-29 01:09:25 +0300

    finish tictactoe

--- js/script.js
+++ js/script.js
@@ -20,7 +20,6 @@ let winningArray = [
 gamePlace.onclick = e => {
     if (e.target.className === 'box') {
         if (turn % 2 === 0) {
-            console.log(e.target);
             e.target.innerHTML = 'X';
             turnWho.innerHTML = 'Ход ноликов (O)';
             e.target.className = 'box clicked';
@@ -45,7 +44,7 @@ checkWinner = () => {
             result.innerHTML += 'Победили нолики!';
            endGame();
         } else {
-            console.log(winningArray.length);
+            noWin();
         }
     }
 };
@@ -61,4 +60,12 @@ endGame = () => {
     setTimeout(fadeOut, 2000);
     setTimeout(gameDisabled, 3000);
     restart.style.display = 'block';
+};
+
+// Если ничья, игра предложит начать заново
+noWin = () => {
+    if (document.all[15].className === 'box clicked' && document.all[16].className === 'box clicked' && document.all[17].className === 'box clicked' && document.all[18].className === 'box clicked' && document.all[19].className === 'box clicked' && document.all[20].className === 'box clicked' && document.all[21].className === 'box clicked' && document.all[22].className === 'box clicked' && document.all[23].className === 'box clicked') {
+        restart.style.display = 'block';
+        turnWho.style.display = 'none';
+    }
 };
